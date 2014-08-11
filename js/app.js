81746df6c280e6c4e03fcf89538e64a638bf407e
soygcm <gcmelmejor@gmail.com>
2014-08-11 15:24:05 -0600

    Ocultar barra en el login

--- js/app.js
+++ js/app.js
@@ -25,17 +25,22 @@ AppView = Parse.View.extend({
 	},
 
 	showAdmin: function () {
-		this.mainnav.render();
-		this.$body.addClass(this.loggedClass);
-		this.login.$el.html('');
-		this.admin.render();
+
+		if ( Parse.User.current() ) {
+
+			this.mainnav.render().$el.show();
+			this.$body.addClass(this.loggedClass);
+			this.login.$el.html('');
+			this.admin.render();
+
+		}
 	},
 
 	showLogIn: function () {
 		this.$body.removeClass(this.loggedClass);
 		this.login.render();
 		this.admin.$el.html('');
-		this.mainnav.$el.html('');
+		this.mainnav.$el.html('').hide();
 	},
 
 	editBook: function(id) {
