517cc6bccc547877ad6a32af90dd30fd0dff9399
soygcm <gcmelmejor@gmail.com>
2015-05-05 15:38:15 -0600

    contadores y otras cosas

---
+++ js/index.js
@@ -0,0 +1,78 @@
+/*Echo@@19900*/
+
+/*http://findbooks.com*/ 
+
+/*
+http://www.inc.com/drew-hendricks/50-websites-your-startup-needs-to-succeed.html
+
+http://www.inc.com/drew-hendricks/50-websites-your-startup-needs-to-succeed.html
+
+Gradiente
+http://www.css3maker.com/css-gradient.html
+
+http://www.cssdrive.com/imagepalette/index.php
+*/
+
+/*Formularios
+http://www.jotform.com/form-templates/
+*/
+
+
+/*
+https://onepagelove.com/gallery/iphone-app/page/4
+
+http://beardswipe.me/#
+
+http://hinge.co/
+
+http://www.excitoo.com/
+
+http://labcase.org/
+
+
+http://ffc2015.startupnotes.org/#page/1
+
+https://www.yappexperience.com/
+
+http://www.aulaya.com/
+
+http://shortcutapp.com/
+
+https://www.avuba.de/#/
+
+
+
+
+IMPORTANT:
+
+https://www.instapage.com/2015/03/03/6-psychological-principles-persuade-visitors-convert/
+
+http://boligreisen.no/lete
+
+http://www.adoratorio.com/
+http://www.giantstepsmedias.com/
+*/
+
+
+  
+  /*background: url(https://farm4.staticflickr.com/3798/13069885703_8a180fc88a_b.jpg) no-repeat center fixed;
+  Other images to use
+https://farm9.staticflickr.com/8250/8455515634_0d4224fce3_b.jpg 
+  
+https://farm8.staticflickr.com/7045/7048976671_8bdc811b78_b.jpg
+  
+https://farm8.staticflickr.com/7361/9434064193_e54834135e_b.jpg
+
+  
+https://farm1.staticflickr.com/77/160696504_0653f1dc91_b.jpg
+  https://farm4.staticflickr.com/3211/2497834000_73aaaa548f_b.jpg
+    https://farm4.staticflickr.com/3177/13069877453_9afb090008_b.jpg
+    https://farm4.staticflickr.com/3798/13069885703_8a180fc88a_b.jpg
+  https://farm3.staticflickr.com/2725/13091691603_3cff85b97e_b.jpg
+
+  https://farm4.staticflickr.com/3250/3015868721_fc53681774_b.jpg
+  https://farm9.staticflickr.com/8015/7658034524_cea1c4ddba_b.jpg
+
+  
+    https://www.flickr.com/search/?q=reader&text=sharing%20books
+  */
